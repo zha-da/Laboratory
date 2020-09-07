21bbf6be9517c9bd743de5b7f9c074d34e79b3f1
Zhavoronkov <Zhavoronkov@PC415-06>
2020-09-07 12:20:40 +0300

    Added ExamQuestion

--- Lab/Exam.cs
+++ Lab/Exam.cs
@@ -9,11 +9,13 @@ namespace Lab
     /// <summary>
     /// Класс экзамена
     /// </summary>
-    class Exam
+    public class Exam
     {
         #region Fields
         private int exTime;
-
+        /// <summary>
+        /// Длительность экзамена
+        /// </summary>
         public int ExamTime
         {
             get { return exTime; }
@@ -24,37 +26,128 @@ namespace Lab
             }
         }
         private string discip;
-
+        /// <summary>
+        /// Название дисциплины
+        /// </summary>
         public string Discipline
         {
             get { return discip; }
             set { discip = value; }
         }
         private int maxSc;
-
+        /// <summary>
+        /// Максимальная отметка (не может быть ниже 0).
+        /// </summary>
         public int MaximumScore
         {
             get { return maxSc; }
-            set { maxSc = value; }
+            set
+            {
+                if (value < 0) return;
+                maxSc = value;
+            }
         }
+        private int quetsQuan;
+        /// <summary>
+        /// Количество вопросов
+        /// </summary>
+        public int QuestionsQuantity
+        {
+            get { return quetsQuan; }
+            set { quetsQuan = value; }
+        }
+        /// <summary>
+        /// Вопросы к экзамену
+        /// </summary>
+        public List<ExamQuestion> Questions { get; set; }
         #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Создает экземпляр класса с параметрами по умолчанию
+        /// </summary>
         public Exam()
         {
             exTime = 45;
             discip = "Не выбрано";
             maxSc = 5;
         }
+        /// <summary>
+        /// Создает экземпляр класса
+        /// </summary>
+        /// <param name="discipline">Название дисциплины</param>
         public Exam(string discipline)
         {
             exTime = 45;
             this.discip = discipline;
             maxSc = 5;
         }
+        /// <summary>
+        /// Создает экземпляр класса
+        /// </summary>
+        /// <param name="maximumScore">Максимальная отметка за экзамен</param>
+        /// <param name="discipline">Название дисциплины</param>
+        /// <param name="examTime">Длительность экзамена</param>
         public Exam(int maximumScore, string discipline, int examTime)
         {
             maxSc = maximumScore;
             discip = discipline;
             exTime = examTime;
         }
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Выводит всю информацию об экзамене
+        /// </summary>
+        public void DisplayInfo()
+        {
+
+        }
+        /// <summary>
+        /// Проводит экзамен у группы учеников
+        /// </summary>
+        public void TakeAnExam( )
+        {
+
+        }
+        /// <summary>
+        /// Определяет итоговую оценку
+        /// </summary>
+        public double CalculateMark()
+        {
+            double res = 0;
+            foreach (ExamQuestion q in Questions)
+            {
+                if (q.AnsweredCorrectly) res++;
+            }
+            return res / quetsQuan;
+        }
+        #endregion
+
+        #region Operators
+        /// <summary>
+        /// Сравнивает 2 экзамена по количеству вопросов
+        /// </summary>
+        /// <param name="e1">Экзамен 1</param>
+        /// <param name="e2">Экзамен 2</param>
+        /// <returns>Результат сравнения по количеству вопросов</returns>
+        public static bool operator > (Exam e1, Exam e2)
+        {
+            return e1.quetsQuan > e2.quetsQuan;
+        }
+        /// <summary>
+        /// Сравнивает 2 экзамена по количеству вопросов
+        /// </summary>
+        /// <param name="e1">Экзамен 1</param>
+        /// <param name="e2">Экзамен 2</param>
+        /// <returns>Результат сравнения по количеству вопросов</returns>
+        public static bool operator < (Exam e1, Exam e2)
+        {
+            return e1.quetsQuan < e2.quetsQuan;
+        }
+        #endregion
     }
 }
