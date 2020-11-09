14f73cba9331bf1b5592dba05be4d478a7476855
_dan <zhavoronkovdan@gmail.com>
2020-11-09 15:41:16 +0300

    Created WPF Testing App

---
+++ LabTesting/ViewModels/SemesterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LabTesting.ViewModels
+{
+    class SemesterViewModel : Screen
+    {
+        private int _take = 0;
+
+        public int Take
+        {
+            get { return _take; }
+            set
+            {
+                _take = value;
+                NotifyOfPropertyChange(() => Take);
+                NotifyOfPropertyChange(() => CanShowMessage);
+            }
+        }
+
+        public bool CanShowMessage
+        {
+            get => Take == 0;
+        }
+        public void ShowMessage()
+        {
+            MessageBox.Show("f");
+            Take++;
+        }
+    }
+}
