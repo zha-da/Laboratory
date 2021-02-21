9b7729bdd73f9012ff363befa331df3598501548
_dan <zhavoronkovdan@gmail.com>
2021-02-21 19:25:01 +0300

    /lab1 complete/

---
+++ LaboratoryMain/UDirectory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryMain
+{
+    [Serializable]
+    public class UDirectory
+    {
+        public DateTime CreationTime { get; set; }
+        public UDirectory[] SubDirectories { get; set; }
+        public UFile[] Files { get; set; }
+        public string Name { get; set; }
+
+        public UDirectory(string path)
+        {
+            CreationTime = new DirectoryInfo(path).CreationTime;
+            Name = path.Substring(path.LastIndexOf('\\') + 1);
+            Files = GetFilesR(path).ToArray();
+            SubDirectories = GetDirectoriesR(path).ToArray();
+        }
+        public UDirectory() { }
+
+        private IEnumerable<UDirectory> GetDirectoriesR(string root)
+        {
+            foreach (var dir in Directory.GetDirectories(root))
+            {
+                var dirInfo = new DirectoryInfo(dir);
+                var directory = new UDirectory
+                {
+                    CreationTime = dirInfo.CreationTime,
+                    Name = dirInfo.Name,
+                    Files = GetFilesR(dir).ToArray(),
+                    SubDirectories = GetDirectoriesR(dir).ToArray()
+                };
+                yield return directory;
+            }
+        }
+
+        private IEnumerable<UFile> GetFilesR(string dir)
+        {
+            foreach (var file in Directory.GetFiles(dir))
+            {
+                var fInfo = new FileInfo(file);
+
+                yield return new UFile
+                {
+                    Data = File.ReadAllBytes(file),
+                    Name = fInfo.Name,
+                    ModificationTime = fInfo.LastWriteTime,
+                    Size = fInfo.Length,
+                    Attributes = fInfo.Attributes
+                };
+            }
+        }
+    }
+}
---
+++ LaboratoryMain/UFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryMain
+{
+    [Serializable]
+    public class UFile
+    {
+        private long _size;
+
+        public string Name { get; set; }
+        public long Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+        public DateTime ModificationTime { get; set; }
+        public System.IO.FileAttributes Attributes { get; set; }
+        public byte[] Data { get; set; }
+
+        public UFile() { }
+    }
+}
