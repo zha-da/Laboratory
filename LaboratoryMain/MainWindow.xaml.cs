eb2666823f844a31d544e8a0a524a3ee1898ba04
_dan <zhavoronkovdan@gmail.com>
2021-03-23 22:39:53 +0300

    /lab4/

--- LaboratoryMain/MainWindow.xaml.cs
+++ LaboratoryMain/MainWindow.xaml.cs
@@ -18,6 +18,9 @@ using System.Xml.Serialization;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
+using System.Threading;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace LaboratoryMain
 {
@@ -26,170 +29,90 @@ namespace LaboratoryMain
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string s_flag = ".dat";
-        public string SFlag 
-        {
-            get => s_flag;
-            set
-            {
-                s_flag = value;
-                ExtBox.Text = value;
-            }
-        }
+        string path;
+        bool needssaving = false;
+        byte needsvisibility = 0;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            s_bin.Click += (s, e) => SFlag = ".dat";
-            s_xml.Click += (s, e) => SFlag = ".xml";
-            s_json.Click += (s, e) => SFlag = ".json";
         }
-        string folderpath;
-        UDirectory directory;
-        private void ChooseBth_Click(object sender, RoutedEventArgs e)
+
+        private void tbword_TextChanged(object sender, TextChangedEventArgs e)
         {
-            FolderBrowserDialog fbd = new FolderBrowserDialog();
-            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            string line = tbword.Text;
+
+            if (String.IsNullOrEmpty(line)) return;
+
+            Regex rg = new Regex(@line);
+            MatchCollection mc = rg.Matches(rtbFile.Text);
+            
+            foreach (Match m in mc)
             {
-                folderpath = fbd.SelectedPath;
-                directory = new UDirectory(folderpath);
+                rtbFile.Select(m.Index, m.Length);
+                rtbFile.Focus();
             }
         }
-
-        private void SerializeBth_Click(object sender, RoutedEventArgs e)
+        private async void bchoose_Click(object sender, RoutedEventArgs e)
         {
-            string filename;
-            System.Windows.Forms.OpenFileDialog fbd = new System.Windows.Forms.OpenFileDialog();
-            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                filename = fbd.FileName;
-            }
-            else return;
-
-            Serialize(directory, filename);
-
-            System.Windows.Forms.MessageBox.Show("Сериализация завершена");
-        }
-
-        private void DeserializeBtn_Click(object sender, RoutedEventArgs e)
-        {
-            string filename;
             System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
             if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                filename = ofd.FileName;
-            }
-            else return;
+                rtbFile.Clear();
+                path = ofd.FileName;
 
-            UDirectory dir = Deserialize(filename);
+                //string[] fulltext = (new StreamReader(path)).ReadToEnd().Split('\n');
 
-            string path;
-            FolderBrowserDialog fbd = new FolderBrowserDialog();
-            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                path = fbd.SelectedPath;
-            }
-            else return;
-
-            CreateFolders(dir, path);
-
-            System.Windows.Forms.MessageBox.Show("Десериализация завершена");
-        }
-
-        private void CreateFolders(UDirectory dir, string path)
-        {
-            DirectoryInfo dinfo = new DirectoryInfo(path);
-            dinfo.CreateSubdirectory(dir.Name);
-
-            dinfo = new DirectoryInfo($"{path}\\{dir.Name}");
-            dinfo.CreationTime = dir.CreationTime;
-
-            foreach (var item in dir.Files)
-            {
-                CreateFiles(item, $"{path}\\{dir.Name}\\{item.Name}");
-            }
-            foreach (var item in dir.SubDirectories)
-            {
-                CreateFolders(item, $"{path}\\{dir.Name}");
-            }
-        }
-        private void CreateFiles(UFile file ,string path)
-        {
-            File.WriteAllBytes(path, file.Data);
-
-            FileInfo fi = new FileInfo(path);
-            try
-            {
-            fi.Attributes = file.Attributes;
-            fi.LastWriteTime = file.ModificationTime;
-        }
-            catch (UnauthorizedAccessException)
-            {
-                using (StreamWriter sw = new StreamWriter("log.txt"))
+                await Task.Run(() =>
                 {
-                    sw.WriteLine($"У файла {path} невозможно изменить свойства; причина - отказано в доступе");
-                }
-            }
-            catch (SystemException ex)
-            {
-                using (StreamWriter sw = new StreamWriter("log.txt"))
-                {
-                    sw.WriteLine($"У файла {path} невозможно изменить свойства; причина - {ex.Message}");
-                }
-            }
-        }
-        private void Serialize(UDirectory dir, string filename)
-        {
-            switch (SFlag)
-            {
-                case ".dat":
-                    BinaryFormatter bf = new BinaryFormatter();
-                    using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
+                    using (StreamReader sr = File.OpenText(path))
                     {
-                        bf.Serialize(fs, directory);
+                        int lines = 0;
+                        string nl;
+                        while ((nl = sr.ReadLine()) != null)
+                        {
+                            Dispatcher.BeginInvoke(new ThreadStart(delegate { rtbFile.AppendText(nl + "\n"); }));
+                            lines++;
+                            Thread.Sleep(3);
+                        }
+                        Dispatcher.BeginInvoke(new ThreadStart(delegate { tbLines.Text = "Количество строк : " + lines; }));
                     }
-                    return;
-                case ".xml":
-                    XmlSerializer serializer = new XmlSerializer(typeof(UDirectory));
-                    using (FileStream fs = new FileStream(filename, FileMode.OpenOrCreate))
-                    {
-                        serializer.Serialize(fs, directory);
-                    }
-                    return;
-                case ".json":
-                    string res = JsonSerializer.Serialize<UDirectory>(dir);
-                    using (StreamWriter sw = new StreamWriter(filename))
-                    {
-                        sw.WriteLine(res);
-                    }
-                    return;
-                default:
-                    break;
+                });
+                Title = path;
+                GC.Collect();
             }
         }
-        private UDirectory Deserialize(string filename)
+        void AddLine(string line) => rtbFile.AppendText(line);
+
+        private void rtbFile_TextChanged(object sender, TextChangedEventArgs e)
         {
-            FileInfo fi = new FileInfo(filename);
-            switch (fi.Extension)
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!needssaving || Title.Equals("MainWindow")) return;
+
+            //using (StreamWriter sw = new StreamWriter(Title))
+            //{
+            //    sw.
+            //}
+        }
+
+        private void bfind_Click(object sender, RoutedEventArgs e)
+        {
+            needsvisibility++;
+            needsvisibility %= 2;
+
+            if (needsvisibility == 1)
             {
-                case ".dat":
-                    BinaryFormatter bf = new BinaryFormatter();
-                    using (FileStream fs = new FileStream(filename, FileMode.Open))
-                    {
-                        return (UDirectory)bf.Deserialize(fs);
-                    }
-                case ".xml":
-                    XmlSerializer xf = new XmlSerializer(typeof(UDirectory));
-                    using (FileStream fs = new FileStream(filename, FileMode.Open))
-                    {
-                        return (UDirectory)xf.Deserialize(fs);
-                    }
-                case ".json":
-                    using (StreamReader sr = new StreamReader(filename))
-                    {
-                        return JsonSerializer.Deserialize<UDirectory>((sr.ReadToEnd()).ToString());
-                    }
-                default: return null;
+                tbword.Visibility = Visibility.Visible;
+                tbword.Focus();
+            }
+            else
+            {
+                tbword.Visibility = Visibility.Collapsed;
+                tbword.Text = "";
             }
         }
     }
---
+++ Regex/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace RegexLab
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Открыть файл? (Y|N) ");
+                if (Console.ReadLine().ToLower().Equals("n"))
+                {
+                    break;
+                }
+
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() != DialogResult.OK)
+                {
+                    continue;
+                }
+
+                StreamReader sr = new StreamReader(ofd.FileName);
+                string fulltext = sr.ReadToEnd();
+                sr.Close();
+                sr.Dispose();
+
+                Console.WriteLine(fulltext);
+
+                Console.Write("Введите слово для проверки : ");
+                string src = Console.ReadLine();
+
+                Regex regex = new Regex(src);
+                MatchCollection mc = regex.Matches(fulltext);
+
+                if (mc.Count % 10 == 1 & (mc.Count % 100) / 10 != 1)
+                {
+                    Console.WriteLine($"В файле найдено {mc.Count} совпадение");
+                }
+                else if ((mc.Count % 10 > 1 | mc.Count < 5) & (mc.Count % 100) / 10 != 1)
+                {
+                    Console.WriteLine($"В файле найдено {mc.Count} совпадения");
+                }
+                else Console.WriteLine($"В файле найдено {mc.Count} совпадений");
+
+                Console.Write("Заменить даты по условию? (каждая дата в тексте заменяется на следующую) (Y|N) ");
+                if (Console.ReadLine().ToLower().Equals("n"))
+                {
+                    continue;
+                }
+
+                regex = new Regex(@"(0[1-9]|[12][0-9]|3[01])[.,](0[1-9]|1[012])[.,](?:19[0-9]{2}|20(?:[01][0-9]|2[01]))");
+                mc = regex.Matches(fulltext);
+                if (mc.Count != 0)
+                {
+                    StringBuilder replaces = new StringBuilder(128);
+                    foreach (Match match in mc)
+                    {
+                        string repres = ChangeDate(match);
+                        if (string.IsNullOrEmpty(repres)) continue;
+                        replaces.Append($"{match} заменено на {repres}\n");
+                        fulltext = fulltext.Replace(match.Value, repres);
+                    }
+
+                    MessageBox.Show(replaces.ToString());
+
+                    Console.WriteLine(fulltext); 
+                }
+                else
+                {
+                    MessageBox.Show("В тексте нет подходящих дат");
+                }
+
+                Console.Write("Продолжить? (Y|N) ");
+                if (Console.ReadLine().ToLower().Equals("n"))
+                {
+                    break;
+                }
+            }
+        }
+
+        private static string ChangeDate(Match match)
+        {
+            //string[] nums = match.ToString().Split('.', ',');
+
+            //List<string> months = new List<string>(new string[] { "01", "03", "05", "07", "08", "10", "12" });
+
+            DateTime dateres;
+            if (DateTime.TryParse(match.ToString(), out dateres))
+            {
+                dateres = dateres.AddDays(1);
+                return dateres.ToShortDateString();
+            }
+            return null;
+        }
+    }
+}
